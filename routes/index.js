7dd61b73e7b430909af29a1f43ed965dd8f890d4
emre.kaplan <ekaplan@kitapyurdu.com>
2019-06-23 14:16:06 +0300

    welcome page

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,9 @@ var recordController = require('../contollers/recordController');
 
 router.post('/getRecords', recordController.getRecords);
 
+router.get('/', function(req, res){
+  res.send('Welcome. <a href="https://github.com/emrekap/getir-case-study">Github Repo</a> contains .env file for ease of use.');
+});
+
+
 module.exports = router;
