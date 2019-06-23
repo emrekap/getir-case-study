ce3290412112389193cba7a6cb5b2d4cca5dd6ab
emre.kaplan <ekaplan@kitapyurdu.com>
2019-06-23 12:54:53 +0300

    new added test. npm start changed

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,7 +3,7 @@ const app = require('../app.js')
 describe('Test getRecords', () => {
     test('It should response the POST method', () => {
         return request(app).post("/getRecords").then(response => {
-            console.log(response.body);
+            //console.log(response.body);
             expect(response.statusCode).toBe(200)
             expect(response.body.code).toBe(0);
             expect(response.body.msg).toBe('Success');
@@ -25,6 +25,21 @@ describe('Test getRecords', () => {
         }).catch(err => {
             console.log(err);
         })
+    });
+
+    test('It should greater or equal 3000 and less than or equal 3500 response the POST method', () => {
+        return request(app).post("/getRecords").send({"minCount": 3000, "maxCount": 3500}).then(response => {
+            //console.log(response.body.records);
+            if(response.body.records){
+                response.body.records.map(record=> {
+                    expect(record.totalCount).toBeLessThanOrEqual(3500);
+                    expect(record.totalCount).toBeGreaterThanOrEqual(3000);
+                });
+            }
+        }).catch(err => {
+            console.log(err);
+        })
+
     });
 
 })
